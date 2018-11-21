447a9b544c5972f7cedb90909655bd67c195504e
william-eyre <william.eyre@sainsburys.co.uk>
2018-11-21 10:51:30 +0000

    food item container is now one class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.scss';
 import {
   Header
@@ -11,32 +11,39 @@ class App extends Component {
   render() {
     return (
         <div className="App">
-          <div style={{ overflow: 'hidden', minHeight: '12rem' }}>
+          <div style={{overflow: 'hidden', minHeight: '12rem'}}>
             <Header
                 iconNav={[
-                  <a href="#settings" title="Settings" onClick={e => e.preventDefault()}>
-                    <Settings />
+                  <a href="#settings" title="Settings"
+                     onClick={e => e.preventDefault()}>
+                    <Settings/>
                   </a>,
-                  <a href="#user" title="Profile" onClick={e => e.preventDefault()}>
-                    <OverflowMenu />
+                  <a href="#user" title="Profile"
+                     onClick={e => e.preventDefault()}>
+                    <OverflowMenu/>
                   </a>,
                 ]}
                 topBar={{
                   logo: (
-                      <Sainsburys height="25" width="95" className="ln-u-display-block" />
+                      <Sainsburys height="25" width="95"
+                                  className="ln-u-display-block"/>
                   ),
                   items: [<a href="#more">Healthy Eating Hackathon</a>],
                 }}
-                style={{ position: 'static' }}
+                style={{position: 'static'}}
             />
           </div>
           <body>
           <div>
-            <FoodItemContainer/>
-            <FoodItemContainer/>
-            <FoodItemContainer/>
-            <FoodItemContainer/>
-            <FoodItemContainer/>
+            <FoodItemContainer foodItem="Cheese"
+                               healthyOption="Low fat Cheese"/>
+            <FoodItemContainer foodItem="Yoghurt"
+                               healthyOption="Low fat yoghurt"/>
+            <FoodItemContainer foodItem="Coke" healthyOption="Diet Coke"/>
+            <FoodItemContainer foodItem="Full Fat Milk"
+                               healthyOption="Semi-skimmed milk"/>
+            <FoodItemContainer foodItem="Baked Beans"
+                               healthyOption="Reduced sugar beans"/>
           </div>
           </body>
         </div>
--- src/foodItem/FoodItemContainer.js
+++ src/foodItem/FoodItemContainer.js
@@ -1,20 +1,47 @@
 import React from "react"
-import FoodItem from "./FoodItem";
-import {FilledButton} from "luna-react";
+import {Card, FilledButton} from "luna-react";
 import "./foodItemContainer.scss";
 
 class FoodItemContainer extends React.Component {
 
+  state = {
+    swapItem: false
+  };
+
+
+  swapItem = () => {
+
+  };
+
+
   render() {
+    const foodItem = this.props.foodItem;
+    const healthyOption = this.props.healthyOption;
+
+
     return (
-        <div className="food-item-container">
-          <FoodItem/>
+        <div className="food-item-container" onClick={this.swapItem}>
+          <Card>
+            <div>
+              <img src={this.props.imgSrc} alt="unhealthyOption"/>
+            </div>
+            <h3>{foodItem}</h3>
+            <p></p>
+          </Card>
+
           <div className="swap-button">
             <FilledButton>
               Swap
             </FilledButton>
           </div>
-          <FoodItem/>
+
+          <Card>
+            <div>
+              <img src={this.props.healthyImgSrc} alt="healthyOption"/>
+            </div>
+            <h3>{healthyOption}</h3>
+            <p></p>
+          </Card>
         </div>
     )
   }
